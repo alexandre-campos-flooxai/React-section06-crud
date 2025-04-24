12427cae117d2453db0f1c61ea1e7f9494d67e1f
AlexandreCampos <alexandre.campos.flooxai@gmail.com>
2025-04-24 16:08:35 -0300

    feat: create class client and table

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,3 +1,24 @@
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+
 export default function Home() {
-  return <span className="flex  ">Texto</span>;
+  const clientes = [
+    new Cliente("Alex", 32, "1"),
+    new Cliente("Paulo", 35, "2"),
+    new Cliente("Ricardo", 20, "3"),
+    new Cliente("João", 45, "4"),
+    new Cliente("Kaio", 32, "5"),
+  ];
+  return (
+    <span
+      className={`flex justify-center items-center h-screen
+    bg-gradient-to-r from-blue-500 to-purple-500
+    text-white`}
+    >
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
+    </span>
+  );
 }
